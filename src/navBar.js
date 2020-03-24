0e37bee49c8bc526b98b3f7761be48ced4548a2c
Frederik De Cooman <frederik.decooman@student.odisee.be>
2020-03-24 14:50:00 +0100

    added customization

--- src/navBar.js
+++ src/navBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
-const NavBar = () => <AppBar title="Ikdoeict AWS Resource overview" />;
+const NavBar = () => <AppBar title="Ikdoeict AWS Resource overview by FDC" />;
 
 export default NavBar;
